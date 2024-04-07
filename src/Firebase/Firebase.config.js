eff2ff37d5bcf60708839b9400f6126c3d436820
Humayra-Amin <humaema4576@gmail.com>
2024-04-08 01:43:27 +0600

    environment set

--- src/Firebase/Firebase.config.js
+++ src/Firebase/Firebase.config.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAbZKFrvQw1fcIInpixo21bOeqHi-PE6bU",
-  authDomain: "a9-luxury-real-estate.firebaseapp.com",
-  projectId: "a9-luxury-real-estate",
-  storageBucket: "a9-luxury-real-estate.appspot.com",
-  messagingSenderId: "599517252457",
-  appId: "1:599517252457:web:b0881b5acaf460a5e9cbf8"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
