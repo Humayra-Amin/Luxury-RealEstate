22c189f4001ded1c1b342c7b8f4be8ab3c00a5d5
Humayra-Amin <humaema4576@gmail.com>
2024-04-13 01:29:36 +0600

    loading/spinner added

--- src/components/ProtectedRoute/ProtectedRoute.jsx
+++ src/components/ProtectedRoute/ProtectedRoute.jsx
@@ -1,12 +1,13 @@
 import { Navigate, useLocation } from "react-router-dom";
 import useAuth from "../../hooks/useAuth";
+import Spinner from "../Spinner/Spinner";
 
 const ProtectedRoute = ({ children }) => {
     const { user, loading } = useAuth();
     const location = useLocation();
 
     if(loading){
-        return
+        return <Spinner></Spinner>
     }
 
     if (!user) {
